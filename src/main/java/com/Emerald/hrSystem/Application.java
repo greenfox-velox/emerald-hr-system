f5e5fcf126dbbdf1d3d56e767451218826a93e42
annatorok <mail@annatorok.com>
2016-08-11 14:02:07 +0200

    Add proper CSS to registration page and fix proper path as well

--- src/main/java/com/Emerald/hrSystem/Application.java
+++ src/main/java/com/Emerald/hrSystem/Application.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+      "classpath:/static/" };
+
     public static void main(String[] args) throws Throwable {
-        SpringApplication.run(Application.class, args);
+      SpringApplication.run(Application.class, args);
     }
 }
